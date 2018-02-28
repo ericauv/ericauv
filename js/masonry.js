676a4a1d1ec301c2c9365bcbb56cd4107dca1db2
ericauv <ericauv17@gmail.com>
2018-02-27 21:51:49 -0500

    tried to get infinite scroll to gif_gallery

--- js/masonry.js
+++ js/masonry.js
@@ -1,56 +1,5 @@
-// RESPONSIVE MASONRY //
-//jQuery(document).ready(function($) {
-//    var CollManag = (function() {
-//        var $ctCollContainer = $('.masonry-grid'),
-//        collCnt = 1,
-//        init = function() {
-//            changeColCnt();
-//            initEvents();
-//            initPlugins();
-//        },
-//        changeColCnt = function() {
-//            var w_w = $(window).width();
-//            if( w_w <= 600 ) n = 1;
-//            else if( w_w <= 768 ) n = 2;
-//            else n = 3;
-//        },
-//        initEvents = function() {
-//            $(window).on( 'smartresize.CollManag', function( event ) {
-//                changeColCnt();
-//            });
-//        },
-//        initPlugins = function() {
-//            $ctCollContainer.imagesLoaded( function(){
-//                $ctCollContainer.masonry({
-//                    itemSelector : 'grid-item',
-//                    columnWidth : function( containerWidth ) {
-//                        return containerWidth / n;
-//                    },
-//                    isAnimated : true,
-//                    animationOptions: {
-//                        duration: 400
-//                    }
-//                });
-//            });
-//            $ctCollContainer.colladjust();
-//            $ctCollContainer.find('div.ct-coll-item-multi').collslider();
-//        };
-//        return { init: init };
-//    })();
-//    CollManag.init();
-//}); 
-
 //masonry javascript
 
-//initialize masonry
-var $container = jQuery('#container').masonry({
-			//options
-			itemSelector: '.grid-item',
-			columnWidth: '.grid-sizer',
-			gutter:5,
-			percentposition: true,
-			horizontalOrder: true
-		});
 //-------------DESIGN GRID
 var $containerB = jQuery('#container-b').masonry({
 			//options
@@ -63,16 +12,29 @@ var $containerB = jQuery('#container-b').masonry({
 
 
 //------------- GIFS
-		
+	var $container = jQuery('#container').masonry({
+			//options
+			itemSelector: '.grid-item',
+			columnWidth: '.grid-sizer',
+			gutter:5,
+			percentposition: true,
+			horizontalOrder: true
+		});	
 //relayout after each image loads
-$container.imagesLoaded().progress(function() {
-	$container.masonry('layout');
-});
+//$container.imagesLoaded().progress(function() {
+//	$container.masonry('layout');
+//});
+//
+//$container.imagesLoaded(function(){
+//	$('#container').removeClass('not-loaded');
+//});
+//
+//$(document).ready(function(){
+//				  $container.masonry('layout');
+//				  });
 
-$(document).ready(function(){
-				  $container.masonry('layout');
-				  });
 
+	
 //-----------DESIGN
 		
 //relayout after each image loads
@@ -100,5 +62,3 @@ var $containerV = jQuery('#container-v').masonry({
 $(window).load( function(){
 	$containerV.masonry();
 });
-		
-	
